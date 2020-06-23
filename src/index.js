403bfb440fe2742226e7ac5048cefe4d2c5f71b8
ifer-itcast <yangkangkang@itcast.cn>
2020-06-23 10:08:51 +0800

    使用loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-window.onload = function() {
-    document.querySelector('ul').style.listStyle = 'none';
-    console.log(1);
-};
+function *fn() {
+    yield 1;
+    yield 2;
+    return 3;
+}
+let it = fn();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+
+import "@babel/polyfill";
+const arr = ['a', 'c', 'd'];
+console.log(arr.includes('b'));
--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,42 @@ module.exports = {
             filename: 'index.html',
             template: 'src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|webp|woff|woff2|ttf|svg|eot)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        // 图片大于 5kb 就以路径形式展示，否则 base64
+                        limit: 5 * 1024,
+                        outputPath: 'image',
+                        name: '[name]-[hash:4].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    // 带 eval 不会生成新的映射文件，直接在当前文件中定位
+    devtool: 'cheap-module-eval-source-map'
 };
